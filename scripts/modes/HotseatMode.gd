403da2f2361b490532ca706cd6d8f8ed22f5d5b9
nwrowe <nathaniel.w.rowe@gmail.com>
2026-05-14 19:35:02 -0400

    Add hotseat charge input helpers

--- scripts/modes/HotseatMode.gd
+++ scripts/modes/HotseatMode.gd
@@ -7,6 +7,12 @@ static func is_active(menu_state: int, game_mode: int, game_menu_state: int, rea
 static func can_begin_charge(projectile_active: bool, turn_projectiles: Array[Dictionary], game_over: bool, overlay_open: bool) -> bool:
 	return not projectile_active and turn_projectiles.is_empty() and not game_over and not overlay_open
 
+static func can_begin_keyboard_charge(keyboard_down: bool, keyboard_held: bool, projectile_active: bool, turn_projectiles: Array[Dictionary], game_over: bool, overlay_open: bool) -> bool:
+	return keyboard_down and not keyboard_held and can_begin_charge(projectile_active, turn_projectiles, game_over, overlay_open)
+
+static func should_release_keyboard_charge(keyboard_down: bool, keyboard_held: bool) -> bool:
+	return not keyboard_down and keyboard_held
+
 static func turn_label(current_player: int, turn_timer: float) -> String:
 	return "P%d  %02ds" % [current_player + 1, int(ceil(turn_timer))]
 
