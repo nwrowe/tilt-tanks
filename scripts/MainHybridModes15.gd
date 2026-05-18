32528d0e58dbb8627e6a2f9abfc5b35d07946bc0
nwrowe <nathaniel.w.rowe@gmail.com>
2026-05-18 08:20:47 -0400

    Remove obsolete MainHybridModes15 alias

--- scripts/MainHybridModes15.gd
+++
@@ -1,4 +0,0 @@
-extends "res://scripts/weapons/WeaponRuntimeBridge.gd"
-
-# Legacy compatibility alias. Active weapon runtime behavior now lives in
-# scripts/weapons/WeaponRuntimeBridge.gd while MainGame.gd is being flattened.
