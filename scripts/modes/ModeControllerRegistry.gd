00ad178394108583d15b49d95846110e887ccf2a
nwrowe <nathaniel.w.rowe@gmail.com>
2026-05-18 23:35:27 -0400

    Add mode controller registry scaffold

---
+++ scripts/modes/ModeControllerRegistry.gd
@@ -0,0 +1,16 @@
+extends RefCounted
+class_name ModeControllerRegistry
+
+const MODE_HOTSEAT: String = "hotseat"
+const MODE_REALTIME_SINGLE_PLAYER: String = "realtime_single_player"
+
+static func build_default_controllers(owner_node: Node = null, match_controller: MatchController = null) -> Dictionary:
+	return {
+		MODE_HOTSEAT: HotseatModeController.new(owner_node, match_controller),
+		MODE_REALTIME_SINGLE_PLAYER: RealtimeSinglePlayerModeController.new(owner_node, match_controller)
+	}
+
+static func controller_for_name(controllers: Dictionary, mode_name: String) -> ModeController:
+	if controllers.has(mode_name):
+		return controllers[mode_name] as ModeController
+	return controllers.get(MODE_HOTSEAT, ModeController.new()) as ModeController
